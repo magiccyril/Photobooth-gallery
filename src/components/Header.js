1888fb853a6f10f70917567ffa8800a9e56d9b2c
Cyril Gravelier <magiccyril@gmail.com>
2018-06-17 10:05:56 +0000

    generic version

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,7 @@
 import React from 'react';
 
-import logo from '../assets/img/HelenaEtCyril.svg'
-
 export default (props) => (
   <header className="siteHeader">
-    <h1><img src={logo} className="logo" alt="Hélèna & Cyril" /></h1>
-    <h2>- Photobooth -</h2>
+    <h1>- Photobooth -</h1>
   </header>
 )
